2905258f04643bd925a770ca03089e78ddce6eee
Michael Amundsen <michaelamundsen@gmail.com>
2022-05-17 15:55:46 -0400

    Switching to using alert

--- infra/beeep_repo_status_notifier.go
+++ infra/beeep_repo_status_notifier.go
@@ -10,13 +10,13 @@ type beeepRepoStatusNotifier struct {
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToCommit(path string) (err error) {
-	return beeep.Notify("Remind to git commit", path, "")
+	return beeep.Alert("Remind to git commit", path, "")
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToPush(path string) (err error) {
-	return beeep.Notify("Remind to git push", path, "")
+	return beeep.Alert("Remind to git push", path, "")
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToCommitAndPush(path string) (err error) {
-	return beeep.Notify("Remind to git commit/push", path, "")
+	return beeep.Alert("Remind to git commit/push", path, "")
 }
