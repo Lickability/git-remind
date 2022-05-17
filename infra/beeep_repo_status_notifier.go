d965fa5be1e0615da96b922081eb5d8205f4b1b7
Michael Amundsen <michaelamundsen@gmail.com>
2022-05-17 16:35:24 -0400

    Adjusting how we display notifications

--- infra/beeep_repo_status_notifier.go
+++ infra/beeep_repo_status_notifier.go
@@ -1,7 +1,7 @@
 package infra
 
 import (
-	"github.com/gen2brain/beeep"
+	"os/exec"
 )
 
 var BeeepRepoStatusNotifier = &beeepRepoStatusNotifier{}
@@ -10,13 +10,28 @@ type beeepRepoStatusNotifier struct {
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToCommit(path string) (err error) {
+	osa, err := exec.LookPath("osascript")
+	if err != nil {
+		return err
+	}
+
 	return exec.Command(osa, "-e", `display notification "`+path+`" with title "Remind to git commit" sound name "default"`).Run()
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToPush(path string) (err error) {
+	osa, err := exec.LookPath("osascript")
+	if err != nil {
+		return err
+	}
+
 	return exec.Command(osa, "-e", `display notification "`+path+`" with title "Remind to git push" sound name "default"`).Run()
 }
 
 func (*beeepRepoStatusNotifier) NotifyNeedToCommitAndPush(path string) (err error) {
+	osa, err := exec.LookPath("osascript")
+	if err != nil {
+		return err
+	}
+
 	return exec.Command(osa, "-e", `display notification "`+path+`" with title "Remind to git commit/push" sound name "default"`).Run()
 }
